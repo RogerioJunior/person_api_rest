cd67459600982aed21092e8bfc57913393d563c2
rato0 <rogeriopcjr@gmail.com>
2021-11-03 21:09:48 -0300

    add entitys and enum

---
+++ src/main/java/com/personapi/controller/entity/Phone.java
@@ -0,0 +1,29 @@
+package com.personapi.controller.entity;
+
+import com.personapi.controller.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Data // inserts automatically getter and setters
+@Builder // design pattern object construction
+@AllArgsConstructor // insert constructors
+@NoArgsConstructor // insert constructors
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id generate auto
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
