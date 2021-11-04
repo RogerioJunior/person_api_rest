451247c1728be41638da4b08849cf55eed2e1e5f
rato0 <rogeriopcjr@gmail.com>
2021-11-04 10:51:46 -0300

    Developed person

---
+++ src/main/java/com/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.personapi.service;
+
+import com.personapi.dto.MessageResponseDTO;
+import com.personapi.entity.Person;
+import com.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson( Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
