68215d0dd424a7be4574baaae65824e6f72e307d
rato0 <rogeriopcjr@gmail.com>
2021-11-05 08:14:48 -0300

    add README

---
+++ README.md
@@ -0,0 +1,11 @@
+Digital Innovation: Expert class - Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot
+
+executar o projeto no terminal: mvn spring-boot:run
+
+Visualizar a execução do projeto: http://localhost:8080/api/v1/people
+
+Projeto construido com:
+    Java 17
+    Maven 3.8.3
+    Spring Boot 2.5.6
+
--- src/main/java/com/personapi/service/PersonService.java
+++ src/main/java/com/personapi/service/PersonService.java
@@ -6,22 +6,24 @@ import com.personapi.entity.Person;
 import com.personapi.exception.PersonNotFoundException;
 import com.personapi.mapper.PersonMapper;
 import com.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired)) //create constructor
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+//    @Autowired
+//    public PersonService(PersonRepository personRepository) {
+//        this.personRepository = personRepository;
+//    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
